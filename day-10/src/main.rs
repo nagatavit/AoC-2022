fa08f81fb94d006750d6d0caf99893c73b63482e
Vitor Nagata <nagatavit@gmail.com>
2022-12-14 20:33:03 -0300

    Change day-10 for part 2

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -12,33 +12,67 @@ fn main() {
     let important_cycles: Vec<i32> = vec![20, 60, 100, 140, 180, 220];
     let mut current_cycle: i32 = 0;
     let mut register_x: i32 = 1;
-    let mut sig_strength_sum: i32 = 0;
+    let mut sig_sum: i32 = 0;
 
     for line in lines {
         if let Ok(reading) = line {
             let input = reading.split(' ').collect::<Vec<&str>>();
 
             if input[0] == "addx" {
-                current_cycle += 1;
-                if important_cycles.contains(&current_cycle) {
-                    sig_strength_sum += current_cycle * register_x;
-                }
-                current_cycle += 1;
-                if important_cycles.contains(&current_cycle) {
-                    sig_strength_sum += current_cycle * register_x;
-                }
+                add_cycle(
+                    &mut current_cycle,
+                    register_x,
+                    &mut sig_sum,
+                    &important_cycles,
+                );
+
+                add_cycle(
+                    &mut current_cycle,
+                    register_x,
+                    &mut sig_sum,
+                    &important_cycles,
+                );
                 register_x += input[1].parse::<i32>().unwrap();
             } else {
-                current_cycle += 1;
-                if important_cycles.contains(&current_cycle) {
-                    sig_strength_sum += current_cycle * register_x;
-                }
+                add_cycle(
+                    &mut current_cycle,
+                    register_x,
+                    &mut sig_sum,
+                    &important_cycles,
+                );
             }
-
-            // println!("{}: {}", current_cycle, register_x);
         }
     }
-    println!("{}", sig_strength_sum);
+    println!("signal sum: {}", sig_sum);
+}
+
+fn add_cycle(
+    current_cycle: &mut i32,
+    register_x: i32,
+    sig_sum: &mut i32,
+    important_cycles: &[i32],
+) {
+    let modular_cycle = *current_cycle % 40;
+
+    // println!("{}, {}", current_cycle, register_x);
+
+    if modular_cycle == register_x - 1
+        || modular_cycle == register_x
+        || modular_cycle == register_x + 1
+    {
+        print!("#");
+    } else {
+        print!(".");
+    }
+
+    if (*current_cycle + 1) % 40 == 0 {
+        println!();
+    }
+
+    *current_cycle += 1;
+    if important_cycles.contains(current_cycle) {
+        *sig_sum += *current_cycle * register_x;
+    }
 }
 
 // The output is wrapped in a Result to allow matching on errors
