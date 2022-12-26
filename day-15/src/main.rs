a0b5a55bbe1f95a8c8c7ab5d2bbf411283d93553
Vitor Nagata <nagatavit@gmail.com>
2022-12-25 23:44:36 -0300

    Add day-15 part 2

--- day-15/src/main.rs
+++ day-15/src/main.rs
@@ -36,7 +36,6 @@ impl Sensor {
 
 fn main() {
     // File hosts must exist in current path before this produces output
-    // let lines = match read_lines("src/example-input") {
     let lines = match read_lines("src/input") {
         Ok(it) => it,
         _ => {
@@ -85,7 +84,7 @@ fn main() {
     let line_to_search = 2000000;
     let mut positions_without_a_beacon: HashMap<Coordinate, bool> = HashMap::new();
 
-    for sensor in sensors {
+    for sensor in &sensors {
         if sensor.closest_beacon.coord.1 == line_to_search {
             positions_without_a_beacon.insert(sensor.closest_beacon.coord, false);
         }
@@ -105,13 +104,89 @@ fn main() {
         }
     }
 
-    // println!("{:?}", positions_without_a_beacon);
-
     println!(
         "Positions on line {} without a beacon: {}",
         line_to_search,
         positions_without_a_beacon.values().filter(|v| **v).count()
     );
+
+    let mut distress_x_position: i32 = 0;
+    let mut distress_y_position: i32 = 0;
+
+    // let max_line = 20;
+    let max_line = 4000000;
+
+    let mut possible_distress_locations: Vec<(i32, i32)> = Vec::new();
+
+    for line in 0..=max_line {
+        possible_distress_locations.clear();
+
+        for sensor in &sensors {
+            let y_dist = sensor
+                .coord
+                .calculate_manhattan_distance(&Coordinate(sensor.coord.0, line));
+
+            let search_range = sensor.manhattan_distance - y_dist;
+
+            if search_range < 0 {
+                continue;
+            }
+
+            let start_pos = if sensor.coord.0 - search_range > 0 {
+                sensor.coord.0 - search_range
+            } else {
+                0
+            };
+            let end_pos = if sensor.coord.0 + search_range < max_line {
+                sensor.coord.0 + search_range
+            } else {
+                max_line
+            };
+
+            possible_distress_locations.push((start_pos, end_pos));
+        }
+
+        possible_distress_locations.sort_by_key(|k| (k.0, k.1));
+
+        let mut merged_ranges: Vec<(i32, i32)> = vec![possible_distress_locations[0]];
+
+        for locations in &possible_distress_locations {
+            let mut last_merged = merged_ranges.last_mut().unwrap();
+            if locations.0 <= last_merged.1 || locations.0 == last_merged.1 + 1 {
+                last_merged.1 = max(last_merged.1, locations.1);
+            } else {
+                merged_ranges.push(*locations);
+            }
+        }
+
+        if merged_ranges.len() > 1 {
+            println!("{:?}", possible_distress_locations);
+            println!("{:?}", merged_ranges);
+            println!("Gap at: x: {}, y: {}", merged_ranges[0].1 + 1, line);
+            distress_x_position = merged_ranges[0].1 + 1;
+            distress_y_position = line;
+        }
+
+        if line % 500000 == 0 {
+            println!("progress at: {}", &line);
+        }
+    }
+
+    const TUNNING_MULTIPLIER: u64 = 4000000;
+
+    println!("x: {}, y: {}", distress_x_position, distress_y_position);
+    println!(
+        "x * 4000000 + y: {}",
+        distress_x_position as u64 * TUNNING_MULTIPLIER + distress_y_position as u64
+    );
+}
+
+fn max(a: i32, b: i32) -> i32 {
+    if a > b {
+        a
+    } else {
+        b
+    }
 }
 
 fn abs_value(a: i32) -> i32 {
